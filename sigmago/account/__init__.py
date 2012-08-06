82249855841ce254108c662c748fcb098ca32440
TonySeek <tonyseek@gmail.com>
2012-08-06 16:35:39 +0800

    #1: Created the account's declartive model.

---
+++ sigmago/account/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+from flask.blueprints import Blueprint
+
+
+__all__ = ("models", "views")
+
+app = Blueprint("account", __name__, template_folder="templates",
+                static_folder="static", url_prefix="/account")
