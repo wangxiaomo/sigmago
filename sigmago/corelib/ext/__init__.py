cda52003a95c791ab1c1cb70315dac1e2002d913
TonySeek <tonyseek@gmail.com>
2012-08-27 02:55:23 +0800

    Using the flask-superadmin.

--- sigmago/corelib/ext/__init__.py
+++ sigmago/corelib/ext/__init__.py
@@ -9,6 +9,7 @@ from flask.ext.login import LoginManager
 from flask.ext.oauth import OAuth
 from flask.ext.openid import OpenID
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.superadmin import Admin
 
 from sigmago.corelib.ext.oauth import setup_oauth_remotes, get_remote_app
 
@@ -21,6 +22,8 @@ login_manager = LoginManager()
 oauth = OAuth()
 openid = OpenID()
 db = SQLAlchemy()
+admin = Admin()
+admin.register = functools.partial(admin.register, session=db.session)
 
 #: wraps the "get_remote_app" function to an instance method
 oauth.get_remote_app = functools.partial(get_remote_app, oauth)
@@ -34,3 +37,4 @@ def setup_extensions_with_app(app):
     setup_oauth_remotes(oauth, app.config,
                         namespace=getattr(app, "app_name", None))
     openid.init_app(app)
+    admin.init_app(app)
